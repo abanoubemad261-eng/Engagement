d23696146f21fed1f2042304ff4fc42e1c031d0b
abanoubemad261-eng <abanoubemad261@gmail.com>
2026-08-16 18:20:23 +0300

    Style final page with 10.jpeg background

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './opening.css'
 import './home.css'
 import './location.css'
 import './wishes-private.css'
+import './final.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
